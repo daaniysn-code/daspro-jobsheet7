42ae02c2b5a2258dd7fae633f07900ebdd7526ac
daaniys-code <daaniysn@gmail.com>
2025-10-15 12:14:16 +0700

    (+) modifikasi lulus & tdk

--- SiakadFor07.java
+++ SiakadFor07.java
@@ -4,10 +4,11 @@ public class SiakadFor07 {
 
         Scanner input = new Scanner(System.in);
 
+        int mhs = 10, lulus = 0, tdkLulus = 0;
         double nilai, tertinggi = 0, terendah = 100;
 
         for (int i =1; i<= 10; i++){
-            System.out.println("Masukan Nilai Mahasiswa ke-" + i + ":");
+            System.out.print("Masukan Nilai Mahasiswa ke-" + i + ":");
             nilai = input.nextDouble();
             if (nilai > tertinggi) {
                 tertinggi = nilai;
@@ -15,9 +16,15 @@ public class SiakadFor07 {
             if (nilai < terendah){
                 terendah = nilai;
             }
+            if (nilai >= 60){
+                lulus++;
+            }else {
+                tdkLulus++;
+            }
         }
         System.out.println("Nilai Tertinggi: " + tertinggi);
         System.out.println("Nilai Terendah: " + terendah);
-
+        System.out.println("Banyak mahasiswa yang lulus: "+ lulus);
+        System.out.println("Banyak mahasiswa yang tidak lulus: "+ tdkLulus);
     }
 }
